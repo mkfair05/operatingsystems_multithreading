6f9d09a397335b40285ca0c62828e08ea4445a10
MeghanFair <mkfair05@gmail.com>
2019-02-09 18:58:13 -0800

    Adding comments to indicate implementations

--- pc_mutex_cond_pthread.c
+++ pc_mutex_cond_pthread.c
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define producer_wait_count;  //number of times producer has to wait
+#define consumer_wait_count;  //number of times consumer has to wait
+
+/*
+* Meghan Fair
+* V00839675
+* 
+* UVic CSC 360 - Spring 2019
+* 
+* Implementation of the producer consumer problem using pthreads
+* with mutexes and condition variables.
+*
+*/
+
 int main(int argc, char *argv[]) {
     return 0;
 }
---
+++ pc_mutex_cond_uthread.c
@@ -0,0 +1,16 @@
+/*
+* Meghan Fair
+* V00839675
+* 
+* UVic CSC 360 - Spring 2019
+* 
+* Implementation of the producer consumer problem using uthreads.
+* This file is a copy of of pc_spinlock_uthread.c, but replaces 
+* spinlocks with blocking mutexes and spinning with condition variables.
+* All waiting in pc_spinlock_uthread.c becomes blocking in this program.
+*
+*/
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
