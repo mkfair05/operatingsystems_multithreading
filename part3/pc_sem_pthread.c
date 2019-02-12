0bdc192f27de8db0f695056c322afe866f7e332d
MeghanFair <mkfair05@gmail.com>
2019-02-12 14:26:35 -0800

    Initializing semaphores, figuring out how to start writing program and what to do.

--- part3/pc_sem_pthread.c
+++ part3/pc_sem_pthread.c
@@ -13,25 +13,59 @@ use the pthread flag with gcc to compile this code
 * 
 * The program is an implementation of the producer consumer problem using semaphores.
 */
-#define ITEMS  //the shared resource pool
+int ITEMS;  
+sem_t sem; //the shared resource pool -- generalized semaphore
 #define MAX_ITEMS = 10
+#define MIN_ITEMS = 0
 
 void producer(void);
 void consumer(void);
 
 int main(int argc, int **argv) {
-    printf("hii");
+    sem_init(&sem, 0, 0);  //initialize generalized semaphore at 0
+    //pthread()   //initialize 2 producer threads
+    //pthread()   //initialize 2 consumer threads
+    sem_destroy(&sem);  //destory semaphore before exiting program
+    printf("Semaphore Program Done\n");
     return 0;
 }
 
 // producer function purpose is to add items to the shared resource pool
 void producer() {
     //should sleep when items is full, notified by consumer to wake up when data removed
+    
+
+
+    // do (
+    //    // produce an item
+
+    //     wait(mutex);
+
+    //     //place in buffer
+
+    //     signal(mutex);
+    //     signal(full);
+    // ) while (true);
+
 }
 
 // consumer function purpose is to remove items from the shared resource pool
 void consumer() {
     //should sleep when item is empty, notified by producer to wake up when data added
+
+    // do{
+
+    //    wait(full);
+    //    wait(mutex);
+
+    // // remove item from buffer
+
+    //    signal(mutex);
+    //    signal(empty);
+
+    // // consumes item
+
+    // } while(true);
 }
 
 
