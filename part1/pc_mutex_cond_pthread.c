cf9a3458cf19b78315d1fa73a67c93d316c5b143
MeghanFair <mkfair05@gmail.com>
2019-02-19 16:12:55 -0800

    creation of makefile, creation of pthreads.

--- part1/pc_mutex_cond_pthread.c
+++ part1/pc_mutex_cond_pthread.c
@@ -1,10 +1,9 @@
-#include <pthread.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
 #include <unistd.h>
 
-#define producer_wait_count;  //number of times producer has to wait
-#define consumer_wait_count;  //number of times consumer has to wait
 
 /*
 * Meghan Fair
@@ -17,6 +16,51 @@
 *
 */
 
-int main(int argc, char *argv[]) {
-    return 0;
+
+
+#define producer_wait_count  //number of times producer has to wait
+#define consumer_wait_count  //number of times consumer has to wait
+#define MAX_ITEMS 10
+const int NUM_ITERATIONS = 200;
+const int NUM_CONSUMERS  = 2;
+const int NUM_PRODUCERS  = 2;
+
+int histogram [MAX_ITEMS+1]; // histogram [i] == # of times list stored i items
+
+int items = 0;
+
+void* producer (void* v) {
+  for (int i=0; i<NUM_ITERATIONS; i++) {
+    // TODO
+  }
+  return NULL;
+}
+
+void* consumer (void* v) {
+  for (int i=0; i<NUM_ITERATIONS; i++) {
+    // TODO
+  }
+  return NULL;
+}
+
+int main (int argc, char** argv) {
+    pthread_t cons1, prod1;  //start off with 2 threads
+    const char *message1 = "consumer 1";
+    const char *message2 = "producer 1";
+
+    // TODO: Create Threads and Join
+    pthread_create(&cons1, NULL, consumer, (void *)message1);
+    pthread_create(&prod1, NULL, producer, (void *)message2);
+
+        //start off with null attributes, may need to change
+
+
+
+    // printf ("items value histogram:\n");
+    // int sum=0;
+    // for (int i = 0; i <= MAX_ITEMS; i++) {
+    // printf ("  items=%d, %d times\n", i, histogram [i]);
+    // sum += histogram [i];
+    // }
+    // assert (sum == sizeof (t) / sizeof (pthread_t) * NUM_ITERATIONS);
 }
